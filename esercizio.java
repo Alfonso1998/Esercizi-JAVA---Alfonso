bb6c357b2fd060bff1cb890af494ab753f83f49a
Alfonso Mandato <mandatoalfonso42@gmail.com>
2022-11-21 13:14:40 +0100

    esercizio svolto

---
+++ esercizio.java
@@ -0,0 +1,20 @@
+public class Esercizio{
+    public static void main(String[] args){
+        int n=0;
+        int n1=1;
+        int res=n+n1;
+       
+        n=res;
+         
+        for (int i=0; i<=5; i++){
+            System.out.println(res);
+         n=res;
+         res=n+n1;
+         n1=res;
+         res=n+res;
+            System.out.println( n + " " + n1);
+        }
+        
+
+    }
+}
