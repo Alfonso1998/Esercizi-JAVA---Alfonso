db0baf62530695508a0f1d6119355e18f9c73125
Alfonso Mandato <mandatoalfonso42@gmail.com>
2022-11-21 17:19:08 +0100

    sommatrice

---
+++ Sommatoria.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Sommatoria {
+
+
+    public static void sommatoria(){
+        Scanner scan=new Scanner(System.in);
+        
+        System.out.print("----SOMMATRICE----:\n");
+        System.out.print("Dammi un numero:");
+        int y=scan.nextInt();
+        int sum=0;
+        while(y!=0){
+         sum=sum+y;
+         System.out.println("La somma e': " +" "+ sum);
+         System.out.print("Dammi un numero:");
+         y=scan.nextInt();
+        }
+
+
+    }
+    public static void main(String[] args){
+        sommatoria();
+    }
+}
