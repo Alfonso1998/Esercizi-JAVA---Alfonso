dc8719acf75545ed4c5f7c3da6a1c477b5629fa8
Alfonso Mandato <mandatoalfonso42@gmail.com>
2022-11-21 16:56:28 +0100

    Pari e dispari

---
+++ Paridispari.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Paridispari {
+
+
+
+    public static void PariDispari(){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Dammi un numero:");
+        int n=scan.nextInt();
+        if(n%2==0){
+            System.out.println("Il numero" + " " + n + " " + "e' pari");
+        }else{
+            System.out.println("Il numero" + " " + n + " " + "e' dispari");
+        }
+    }
+    public static void main(String[] args){
+        PariDispari();
+
+    }
+}
