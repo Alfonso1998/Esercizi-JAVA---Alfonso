9e6baf8fafeea5e50092f9c94724ca4c2a8406fb
Alfonso Mandato <mandatoalfonso42@gmail.com>
2022-11-21 16:52:18 +0100

    compreso input corretto

--- Compreso.java
+++ Compreso.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 public class Compreso {
     
-    public static void Compreso(int a,int b,int c){
+    public static void Compreso(){
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Dammi il primo numero");
+        int a=scan.nextInt();
+        System.out.println("Dammi il secondo numero");
+        int b=scan.nextInt();
+        System.out.println("Dammi il terzo numero");
+        int c=scan.nextInt();
         if(b>a && b<c){
             System.out.println("Il numero" + " " + b + " " + "e' compreso tra i numeri" + " " + a + " "+ "e" + " " + c);
         }else{
@@ -9,6 +17,6 @@ public class Compreso {
     }
 
     public static void main(String[] args){
-        Compreso.Compreso(1, 2, 60);
+        Compreso.Compreso();
     }
 }
