104abbbe4398aca4e81c4402b4a69038b058e926
Alfonso Mandato <mandatoalfonso42@gmail.com>
2022-11-22 10:01:57 +0100

    Max intero K

--- MassimoInteroK.java
+++ MassimoInteroK.java
@@ -3,7 +3,23 @@ public class MassimoInteroK {
 
 public static void massimoInteroK(){
     System.out.println("---MaxValueK");
-    
+    System.out.println("Dammi un numero maggiore di 0:  ");
+    Scanner input=new Scanner(System.in);
+    int n=input.nextInt();
+    int k=0;
+    if(n>=0){
+        for( int i=0;i<n;i++){
+            if(k<=n){
+                k=k+i;
+                if(k>n){
+                     break; 
+                }
+                System.out.println(k);
+            }else{
+                break;
+            }
+        }
+    }
 }
 
 
@@ -12,6 +28,6 @@ public static void massimoInteroK(){
 
 
     public static void main(String[] args){
-        massimoInteroK()
+        massimoInteroK();
     }
 }
