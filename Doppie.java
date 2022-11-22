2fe5b6e51da15c1d811c4c37a0f566bc1c431847
Alfonso Mandato <mandatoalfonso42@gmail.com>
2022-11-22 11:34:13 +0100

    Doppie completato

--- Doppie.java
+++ Doppie.java
@@ -5,19 +5,17 @@ public class Doppie {
     public static Boolean doppie(){
         System.out.println("Scrivi qualcosa e ti diro' se ci sono doppie");
         Scanner input=new Scanner(System.in);
-        boolean res=false;
         String str=input.nextLine();
         int k=0;
-        for(int i=0;i<str.length();i++){
+        for(int i=0;i<str.length()-1;i++){
             k++;
-            if(str.charAt(k)==str.charAt(i)){
+            if(str.charAt(i)==str.charAt(k)){
                 System.out.println("La frase ha almeno una doppia");
                 return true; 
-            }
-            
-            return false;  
+            }  
         }  
-      
+        System.out.println("La frase non ha doppie");
+     return false;
         
     }
     
